239d1056b6414ceb495a2b6965b8b2dcba8dbd8d
Sergh_work <psl.ru@mail.ru>
2025-04-06 11:42:28 +0500

    example database code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 
 from sqlalchemy import BigInteger, String, ForeignKey, insert, select
 from sqlalchemy.engine import create_engine
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker, relationship
 
 
 class Base(DeclarativeBase):
@@ -14,6 +14,7 @@ class Trainer(Base):
 
     trainer_id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     name: Mapped[str] = mapped_column(String)
+    group: Mapped[list['Group']] = relationship(back_populates='clients')
 
     def __repr__(self):
         return f'trainer_id: {self.trainer_id}, name: {self.name}'
@@ -25,6 +26,7 @@ class Group(Base):
     client_id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     name: Mapped[str] = mapped_column(String)
     trainer_id: Mapped[BigInteger] = mapped_column(BigInteger, ForeignKey('trainer.trainer_id'))
+    clients: Mapped['Trainer'] = relationship(back_populates='group')
 
 
 async def main():
@@ -37,16 +39,24 @@ async def main():
         trainer_id=123456789,
         name='name'
     )
-    client = Group(
+    client1 = Group(
         client_id=123654987,
         name='hdg',
         trainer_id=123456789
     )
-    with Session(engine, expire_on_commit=False) as session:
-        session.add_all([trainer, client])
+    client2 = Group(
+        client_id=123654988,
+        name='hdgcc',
+        trainer_id=123456789
+    )
+    Session = sessionmaker(engine, expire_on_commit=False)
+    with Session() as session:
+        session.add_all([trainer, client1, client2])
         session.commit()
-        result = session.get(Trainer, 123456789)
-        print(result)
+        stmt = select(Trainer).where(123456789==Trainer.trainer_id)
+        res = session.execute(stmt)
+        for client in res.scalar().group:
+            print(client.name)
 
 
 if __name__ == '__main__':
